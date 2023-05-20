b91178307fa1c96fa9f0491a8c5adeec24f79b8a
Szabolcs Kocsis <szabolcs.kocsis@stud.uni-corvinus.hu>
2023-05-20 23:10:51 +0200

    Add project files.

---
+++ module/Controllers/PrintController.cs
@@ -0,0 +1,44 @@
+﻿using inprogress.dnn.module.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace inprogress.dnn.module.Controllers
+{
+    public class PrintController : Controller
+    {
+        // GET: Print
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                // Fájl feltöltése és adatok kezelése
+
+                // Példa: Fájlnév mentése a modellbe
+                var model = new PrintModel();
+                model.FileName = file.FileName;
+
+                // Átirányítás a részletek nézetre
+                return RedirectToAction("Details", model);
+            }
+
+            // Visszatérés az Index nézetre, ha nem történt fájl feltöltés
+            return View("Index");
+        }
+
+        public ActionResult Details(PrintModel model)
+        {
+            // Adatok kezelése és kalkuláció
+
+            return View(model);
+        }
+    }
+}
---
+++ module/Models/PrintModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace inprogress.dnn.module.Models
+{
+    public class PrintModel
+    {
+        public string FileName { get; set; }
+        public decimal Price { get; set; }
+        public string Color { get; set; }
+    }
+}
